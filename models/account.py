0e8a2357a1863d6d7433fc39c0efec92ca092c69
Tim Best <BestTimothy1@gmail.com>
2016-04-10 02:50:34 -0700

    user model?

---
+++ models/account.py
@@ -0,0 +1,16 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(32), unique=True)
+    username = db.Column(db.String(32), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
--- models/dummy.py
+++
@@ -1,5 +0,0 @@
-from app import db
-
-
-class Dummy(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
