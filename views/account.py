cac60680dcda8903624fdfc7a2c664ab137f7ee5
Tim Best <BestTimothy1@gmail.com>
2016-04-15 23:02:40 -0700

    cleanup account models

--- views/account.py
+++ views/account.py
@@ -1,8 +1,7 @@
-from app import db, api
+from app import api
 from models.account import User
 
 from flask_restful import fields, marshal_with, reqparse, Resource
-
 
 def email(email_str):
     """Return email_str if valid, raise an exception in other case."""
@@ -33,7 +32,7 @@ user_fields = {
 }
 
 
-class User(Resource):
+class UserResource(Resource):
 
     @marshal_with(user_fields)
     def post(self):
@@ -43,8 +42,7 @@ class User(Resource):
 
     @marshal_with(user_fields)
     def get(self, user_id):
-        args = post_parser.parse_args()
-        user = get_or_abort(User, user_id)
+        user = User.query.get_or_404(user_id)
         return user
 
 api.add_resource(User, '/users')
