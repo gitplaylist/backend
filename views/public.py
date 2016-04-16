f4b4ca2efa313eb54dcb6e61c5c0f6f1b4a2e698
Tim Best <BestTimothy1@gmail.com>
2016-04-16 00:38:14 -0700

    class based views

--- views/public.py
+++ views/public.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask.views import MethodView
 
 
 bp = Blueprint('public', __name__)
@@ -7,10 +8,27 @@ bp = Blueprint('public', __name__)
 def index():
     return render_template('public/index.html')
 
-@bp.route('/login')
-def login():
-    return render_template('public/login.html')
+class Signup(MethodView):
+    template = 'public/sign_up.html'
 
-@bp.route('/sign_up')
-def signup():
-    return render_template('public/sign_up.html')
+    def get(self):
+        return render_template(self.template)
+
+    def post(self):
+        form = ''
+        return render_template(self.template)
+
+bp.add_url_rule('/sign_up', view_func=Signup.as_view('sign_up'))
+
+
+class Login(MethodView):
+    template = 'public/login.html'
+
+    def get(self):
+        return render_template(self.template)
+
+    def post(self):
+        form = ''
+        return render_template(self.template)
+
+bp.add_url_rule('/login', view_func=Login.as_view('login'))
