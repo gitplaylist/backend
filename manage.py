a1d0f771cb6d010e23a2b1577794abba661a8926
Stewart Park <hello@stewartjpark.com>
2016-05-24 19:49:05 -0700

    Remove the duplicate commands You can use it with `./manage.py db`.

--- manage.py
+++ manage.py
@@ -4,23 +4,18 @@
 from config import Config
 
 from flask.ext.script import Manager, Server, Shell
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import MigrateCommand
 
 from app import create_app
 
 app = create_app()
 
-migrate = Migrate(app, app.db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 manager.add_command(
     "runserver",
     Server(host=Config.HOST, port=Config.PORT)
-)
-manager.add_command(
-    'migrate',
-    MigrateCommand
 )
 
 if __name__ == '__main__':
