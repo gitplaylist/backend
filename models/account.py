c8f4d164a820e4e2a4e017ccfcaf28286d21f952
Tim <besttimothy1@gmail.com>
2016-05-29 21:12:25 -0700

    remove username

--- models/account.py
+++ models/account.py
@@ -10,7 +10,6 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(32), unique=True)
-    username = db.Column(db.String(32), unique=True)
     password_hash = db.Column(db.String(128))
     date_created = db.Column(db.DateTime)
     date_updated = db.Column(db.DateTime)
