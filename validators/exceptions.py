bd3cd9e54240244d2eefac7d13b2742c188a4c71
Stewart Park <hello@stewartjpark.com>
2016-06-12 20:49:27 -0700

    Fix more formats

--- validators/exceptions.py
+++ validators/exceptions.py
@@ -1,5 +1,7 @@
-
+"""Define exceptions."""
 
 class ValidationError(ValueError):
+    """Define a validation error."""
+
     def __init__(self, message):
         super(ValidationError, self).__init__(message)
