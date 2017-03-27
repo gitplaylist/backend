851c6144521eaef65a372ba5c444fcd5d189b42b
Tim <hello@timbest.net>
2017-03-26 17:20:07 -0700

    remove flask bacvkend

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-# Base image
-FROM python:3-onbuild
-
-# The port number the container should expose
-EXPOSE 80
-
-# run the application
-ENTRYPOINT ["python", "manage.py"]
-CMD ["runserver"]
--- app/exceptions.py
+++
@@ -1,8 +0,0 @@
-"""Define exceptions."""
-
-class ValidationError(ValueError):
-    """Define a validation error."""
-
-    def __init__(self, message):
-        super(ValidationError, self).__init__(message)
-        self.message = message
