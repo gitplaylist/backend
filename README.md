c98dc1282565eb0f4a36f4bcc9030cb0389fc731
Tim <hello@timbest.net>
2017-03-26 19:13:53 -0700

    rustless bulding

--- README.md
+++ README.md
@@ -2,13 +2,24 @@
 
 ## Setup
 
-```bash
-# install rust
-$ curl -sSf https://static.rust-lang.org/rustup.sh | sh
 
-# build project
-$ cargo build
+### install rust
+```$ curl -sSf https://static.rust-lang.org/rustup.sh | sh```
 
-# run project
+### build project
+```$ cargo build```
+
+#### Mac errors & fixes in setup:
+`fatal error: 'openssl/ssl.h' file not found`.
+```
+export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include
+export DEP_OPENSSL_INCLUDE=/usr/local/opt/openssl/include
+```
+
+`note: ld: library not found for -lssl`
+https://github.com/sfackler/rust-openssl
+
+### run project
+```
 $ cargo run
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,65 @@
-
-
+#[macro_use]
+extern crate rustless;
+extern crate hyper;
 extern crate iron;
+extern crate rustc_serialize as serialize;
+extern crate valico;
 
-use iron::prelude::*;
-use iron::status;
+use valico::json_dsl;
+use hyper::status::StatusCode;
+use rustless::{
+    Application, Api, Nesting, Versioning
+};
+use rustless::json::ToJson;
 
 fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }
 
-    let _server = Iron::new(hello_world).http("localhost:3000").unwrap();
-    println!("On 3000");
+    let api = Api::build(|api| {
+        // Specify API version
+        api.version("v1", Versioning::AcceptHeader("chat"));
+        api.prefix("api");
+
+        // Create API for chats
+        api.mount(Api::build(|chats_api| {
+
+            chats_api.after(|client, _params| {
+                client.set_status(StatusCode::NotFound);
+                Ok(())
+            });
+
+            // Add namespace
+            chats_api.namespace("chats/:id", |chat_ns| {
+
+                // Valico settings for this namespace
+                chat_ns.params(|params| {
+                    params.req_typed("id", json_dsl::u64())
+                });
+
+                // Create endpoint for POST /chats/:id/users/:user_id
+                chat_ns.post("users/:user_id", |endpoint| {
+
+                    // Add description
+                    endpoint.desc("Update user");
+
+                    // Valico settings for endpoint params
+                    endpoint.params(|params| {
+                        params.req_typed("user_id", json_dsl::u64());
+                        params.req_typed("id", json_dsl::string())
+                    });
+
+                    endpoint.handle(|client, params| {
+                        client.json(&params.to_json())
+                    })
+                });
+
+            });
+        }));
+    });
+
+    let app = Application::new(api);
+
+    iron::Iron::new(app).http("0.0.0.0:4000").unwrap();
+    println!("On 4000");
+
+    println!("Rustless server started!");
 }
